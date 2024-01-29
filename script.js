b5ab0027c7ffc9b5af2951a81363ce56718fc940
subrata coder <subratacoder@protonmail.com>
2024-01-29 10:37:31 +0530

    update the game mode with computer and player

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const boxs = document.querySelectorAll(".box");
-let turnO = false;
+let yourTurn = true;
+let boxClicked = 0;
 const patarns = [
     [0, 1, 2],
     [3, 4, 5],
@@ -11,47 +12,77 @@ const patarns = [
     [2, 4, 6]
 ];
 
-boxs.forEach(box => {
-    box.addEventListener("click", () => {
-        document.getElementById("clickSound").play();
+// adding game mode
+let buttonName;
+document.querySelectorAll(".btn").forEach(btn => {
+    btn.addEventListener("click", () => {
+        document.getElementById("game_box").style.display = "block";
+        document.querySelector(".mode").style.display = "none";
         
-        // scale the vox on click
-        box.style.scale = "1.1";
-        setTimeout(()=>{
-        box.style.scale = "1";
-        }, 700);
-        
-        if (!turnO) {
-            box.innerText = "X";
-            turnO = true;
-            box.style.color = "#59c3c3";
-        } else {
-            box.innerText = "O";
-            turnO = false;
-            box.style.color = "#a63a50";
-        }
-        box.disabled = true;
-
-        for (let patarn of patarns) {
-            const positionIn1 = boxs[patarn[0]].innerText;
-            const positionIn2 = boxs[patarn[1]].innerText;
-            const positionIn3 = boxs[patarn[2]].innerText;
-
-            if (positionIn1 != "" && positionIn2 != "" && positionIn3 != "") {
-                if (positionIn1 == positionIn2 && positionIn2 == positionIn3) {
-                    showWinner(positionIn1);
-                }
-            }
-        }
+      buttonName =  btn.innerText;
     });
 });
 
+// adding eventlistener for user click
+boxs.forEach(box => {
+    box.addEventListener("click", () => {
+      clickBtn(box);
+      
+      if(buttonName == "COM"){
+         setTimeout(random, 700);
+      }
+      
+    });
+});
+
+// adding fuction for button click feadback
+function clickBtn(box) {
+    document.getElementById("clickSound").play();
+
+    // scale the box on click
+    box.style.scale = "1.1";
+    setTimeout(() => {
+        box.style.scale = "1";
+    }, 700);
+
+    if (yourTurn) {
+        box.innerText = "X";
+        box.style.color = "#59c3c3";
+        yourTurn = false;
+    } else {
+        box.innerText = "O";
+        box.style.color = "#a63a50";
+        yourTurn = true;
+    }
+
+    box.disabled = true;
+    boxClicked++;
+    checkWin();
+}
+
+// adding function for ramdom click
+function random() {
+    randomClick(Math.floor(Math.random() * 9));
+}
+function randomClick(index) {
+    const randomIndex = index;
+
+    if (boxs[randomIndex].disabled) {
+        random();
+    } else {
+        clickBtn(boxs[randomIndex]);
+    }
+}
+
+// adding restart function
 const restartBtns = document.querySelectorAll(".Re_btn");
 
 restartBtns[0].addEventListener("click", Restart);
 restartBtns[1].addEventListener("click", () => {
     Restart();
     document.getElementById("result_box").classList.add("hide");
+    document.querySelector(".mode").style.display = "flex";
+    document.querySelector("#game_box").style.display = "none";
 });
 
 function Restart() {
@@ -59,9 +90,28 @@ function Restart() {
         box.innerText = "";
         box.disabled = false;
     }
-    
+    restartBtns[0].classList.remove("hide");
     // scroll to the game
-    window.scrollTo(0,0);
+    window.scrollTo(0, 0);
+
+    yourTurn = true;
+}
+
+// for show winner
+function checkWin() {
+    for (let patarn of patarns) {
+        const positionIn1 = boxs[patarn[0]].innerText;
+        const positionIn2 = boxs[patarn[1]].innerText;
+        const positionIn3 = boxs[patarn[2]].innerText;
+
+        if (positionIn1 != "" && positionIn2 != "" && positionIn3 != "") {
+            if (positionIn1 == positionIn2 && positionIn2 == positionIn3) {
+                showWinner(` Congatulation winner is ${positionIn1}`);
+            } else if (boxs.length === boxClicked) {
+                showWinner("The Match is Draw");
+            }
+        }
+    }
 }
 
 function showWinner(winner) {
@@ -69,11 +119,13 @@ function showWinner(winner) {
     document.getElementById("result").innerText = `${winner}`;
     document.getElementById("result_box").classList.remove("hide");
     restartBtns[0].classList.add("hide");
- 
-// scroll to the result
-window.scrollBy(0,400);
+
+    // scroll to the result
+    window.scrollBy(0, 400);
 
     for (let box of boxs) {
         box.disabled = true;
     }
+
+    boxClicked = 0;
 }
